becee813558ca59aed91ace50523468f26a347d2
liruizhang <lr.zhang1030@gmail.com>
2023-08-23 16:56:27 +0200

    Add the function to print the name of a employee

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
     print('This is a new line')
+    employee_name('Lirui', 'Zhang')
 
+def employee_name(first_name, last_name):
+    print(f'{first_name} {last_name} say is hello')
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
